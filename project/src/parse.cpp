36c857429c61ac165fd52198cc3988985f4793c9
ekamaemo <ekaterinamaksaeva33@gmail.com>
2026-09-19 17:42:54 +0300

    clean task 1.1

--- project/src/parse.cpp
+++
@@ -1,9 +0,0 @@
-#include<../kit/include/l1.2/parse.h>
-#include<../kit/include/l1.2/event.h>
-
-namespace nano_edr {
-    bool IsBlankOrComment(const std::string* line) {
-        std::size_t i = 0;
-        return true;
-    }
-}
